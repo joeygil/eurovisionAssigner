80ffb551684d3bd505994c47b6a499be3a129336
joeygil <105308164+joeygil@users.noreply.github.com>
2022-05-13 20:12:34 +0100

    create eurovision.py

---
+++ eurovision.py
@@ -0,0 +1,17 @@
+import random
+
+team = ['armenia', 'czech rep', 'germany', 'lithuania', 'poland,', 'spain', 'uk', 'australia', 'estonia', 'greece', 'moldova', 'portugal', 'sweden', 'azerbaijan', 'finland', 'iceland', 'nederlands', 'romania', 'switzerland', 'belgium', 'france', 'italy', 'norway', 'serbia', 'ukraine']
+
+emma = random.choice(team)
+joey = random.choice(team)
+boris = random.choice(team)
+walter = random.choice(team)
+pippa = random.choice(team)
+bracken = random.choice(team)
+
+print(emma)
+print(joey)
+print(boris)
+print(walter)
+print(pippa)
+print(bracken)
