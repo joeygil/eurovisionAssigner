1b7497fa18d9274cbb7a10bd4d7e922ede7e33cf
joeygil <105308164+joeygil@users.noreply.github.com>
2022-05-14 09:41:16 +0100

    replace selector

---
+++ eurovisionSelector.py
@@ -0,0 +1,17 @@
+import random
+
+team = ['armenia', 'czech rep', 'germany', 'lithuania', 'poland', 'spain', 'uk', 'australia', 'estonia', 'greece', 'moldova', 'portugal', 'sweden', 'azerbaijan', 'finland', 'iceland', 'nederlands', 'romania', 'switzerland', 'belgium', 'france', 'italy', 'norway', 'serbia', 'ukraine']
+
+emma = random.choice(team)
+joey = random.choice(team)
+boris = random.choice(team)
+walter = random.choice(team)
+pippa = random.choice(team)
+bracken = random.choice(team)
+
+print(emma + ' will be supported by emma')
+print(joey + ' will be supported by joey')
+print(boris + ' will be supported by boris')
+print(walter + ' will be supported by walter')
+print(pippa + ' will be supported by pippa')
+print(bracken + ' will be supported by bracken')
